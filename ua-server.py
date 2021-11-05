6a87f84e0842972afed551766ac4295eb8f159ef
awojnarek <andy.wojnarek@theatsgroup.com>
2021-11-05 22:07:51 +0000

    Cleaned up some code

--- ua-server.py
+++ ua-server.py
@@ -45,46 +45,39 @@ def multiply(parent, x, y):
 
 async def main():
 
-    # now setup our server
+    # Setup our server
     server = Server()
     await server.init()
-    server.disable_clock()  #for debuging
-    #server.set_endpoint("opc.tcp://localhost:4840/freeopcua/server/")
+    server.disable_clock()
     server.set_endpoint("opc.tcp://0.0.0.0:4840/freeopcua/server/")
-    server.set_server_name("FreeOpcUa Example Server")
-    # set all possible endpoint policies for clients to connect through
+    server.set_server_name("Raspberry Pi Kegerator")
+
+    # Set all possible endpoint policies for clients to connect through
     server.set_security_policy([
                 ua.SecurityPolicyType.NoSecurity,
                 ua.SecurityPolicyType.Basic256Sha256_SignAndEncrypt,
                 ua.SecurityPolicyType.Basic256Sha256_Sign])
 
-    # setup our own namespace
-    uri = "http://examples.freeopcua.github.io"
+    # Setup our own namespace
+    uri = "github.com/awojnarek/opcua-pi"
     idx = await server.register_namespace(uri)
 
-    # create a new node type we can instantiate in our address space
-    dev = await server.nodes.base_object_type.add_object_type(idx, "MyDevice")
+    # Create a new node type we can instantiate in our address space
+    dev = await server.nodes.base_object_type.add_object_type(idx, "Raspberry Pi")
     await (await dev.add_variable(idx, "sensor1", 1.0)).set_modelling_rule(True)
     await (await dev.add_property(idx, "device_id", "0340")).set_modelling_rule(True)
     ctrl = await dev.add_object(idx, "controller")
     await ctrl.set_modelling_rule(True)
     await (await ctrl.add_property(idx, "state", "Idle")).set_modelling_rule(True)
 
-    # populating our address space
-
-    # First a folder to organise our nodes
-    myfolder = await server.nodes.objects.add_folder(idx, "SensorData")
-
-    # instanciate one instance of our device
-    mydevice = await server.nodes.objects.add_object(idx, "Device0001", dev)
-    mydevice_var = await mydevice.get_child([f"{idx}:controller", f"{idx}:state"])  # get proxy to our device state variable
+    # Populating our address space
 
     # create directly some objects and variables
-    myobj = await server.nodes.objects.add_object(idx, "HumidityObj")
-    myvar = await myobj.add_variable(idx, "HumidityValue", 0.0)
-    myvar2 = await myobj.add_variable(idx, "TemperatureValue", 0.0)
-    await myvar.set_writable()    # Set MyVariable to be writable by clients
-    await myvar2.set_writable()    # Set MyVariable to be writable by clients
+    sensorobj = await server.nodes.objects.add_object(idx, "SensorObj")
+    humval = await sensorobj.add_variable(idx, "HumidityValue", 0.0)
+    tempval = await sensorobj.add_variable(idx, "TemperatureValue", 0.0)
+    await humval.set_writable()    # Set MyVariable to be writable by clients
+    await tempval.set_writable()    # Set MyVariable to be writable by clients
 
     # creating a default event object
     # The event object automatically will have members for all events properties
@@ -98,15 +91,14 @@ async def main():
 
         while True:
             await asyncio.sleep(0.1)
-            await server.write_attribute_value(myvar.nodeid, ua.DataValue(sin(time.time())))
             humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
             fahrenheit = (temperature * 1.8) + 32
 
             if humidity is not None:
-                await server.write_attribute_value(myvar.nodeid, ua.DataValue(humidity))
+                await server.write_attribute_value(humval.nodeid, ua.DataValue(humidity))
 
             if temperature is not None:
-                await server.write_attribute_value(myvar2.nodeid, ua.DataValue(fahrenheit))
+                await server.write_attribute_value(tempval.nodeid, ua.DataValue(fahrenheit))
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
